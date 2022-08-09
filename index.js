e9345acf8a65ac739bab3296c1c6a3a200ee1850
Jerrica Bobadilla <jbobadilla27@gmail.com>
2022-08-09 15:39:05 -0700

    update instructions and starter code

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  // Write your code here using if...else
+  // Use the 'feet' variable to compare against
+  // Expect 'feet' to be a number
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  // Write your code here using a ternary operator
+  // Use the 'city' variable to compare against
+  // Expect 'city' to be a string
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  // Write your code here using a switch statement
+  // Use the 'tip' variable to compare against
+  // Expect 'tip' to be a number
 }
